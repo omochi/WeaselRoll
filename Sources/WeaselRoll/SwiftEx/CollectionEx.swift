67c6ab8edb9dc7502741f6446ebfafa493aaefc7
omochimetaru <omochi.metaru@gmail.com>
2019-12-17 16:00:14 +0900

    fix

--- Sources/WeaselRoll/SwiftEx/CollectionEx.swift
+++ Sources/WeaselRoll/SwiftEx/CollectionEx.swift
@@ -1,5 +1,5 @@
 extension Collection {
-    public func getOrNone(index: Index) -> Element? {
+    public func getOrNone(at index: Index) -> Element? {
         guard startIndex <= index, index < endIndex else { return nil }
         return self[index]
     }
