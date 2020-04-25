552a8324a3f74a7375693c46c1f81fea792fbd22
omochimetaru <omochi.metaru@gmail.com>
2020-04-26 00:39:25 +0900

    remove Date operators

--- Sources/WeaselRoll/Date/DateEx.swift
+++ Sources/WeaselRoll/Date/DateEx.swift
@@ -17,24 +17,4 @@ extension Date {
                       timeZone: timeZone,
                       locale: Locale(identifier: "en_US_POSIX"))
     }
-
-    public static func +(a: Date, b: TimeInterval) -> Date {
-        a.addingTimeInterval(b)
-    }
-    
-    public static func +=(a: inout Date, b: TimeInterval) {
-        a = a + b
-    }
-    
-    public static func -(a: Date, b: TimeInterval) -> Date {
-        a.addingTimeInterval(-b)
-    }
-    
-    public static func -=(a: inout Date, b: TimeInterval) {
-        a = a - b
-    }
-    
-    public static func -(a: Date, b: Date) -> TimeInterval {
-        a.timeIntervalSince(b)
-    }
 }
