3ba9745f119d1e852f9687e5d9716d7765df84cc
omochimetaru <omochi.metaru@gmail.com>
2020-07-15 20:09:02 +0900

    output formatting

--- Sources/WeaselRoll/Codable/CodableJSON.swift
+++ Sources/WeaselRoll/Codable/CodableJSON.swift
@@ -1,29 +1,29 @@
 import Foundation
 
 extension JSONDecoder {
-    public static let shared: JSONDecoder = {
+    fileprivate static func create() -> JSONDecoder {
         let c = JSONDecoder()
         if #available(OSX 10.12, iOS 10.0, *) {
             c.dateDecodingStrategy = .iso8601
         }
         return c
-    }()
+    }
 }
 
 extension JSONEncoder {
-    public static let shared: JSONEncoder = {
+    fileprivate static func create(outputFormatting: JSONEncoder.OutputFormatting) -> JSONEncoder {
         let c = JSONEncoder()
-        c.outputFormatting = .prettyPrinted
+        c.outputFormatting = outputFormatting
         if #available(OSX 10.12, iOS 10.0, *) {
             c.dateEncodingStrategy = .iso8601
         }
         return c
-    }()
+    }
 }
 
 extension Decodable {
     public static func decodeFromJSON(data: Data) throws -> Self {
-        return try JSONDecoder.shared.decode(Self.self, from: data)
+        return try JSONDecoder.create().decode(Self.self, from: data)
     }
     
     public static func loadFromJSON(file: URL) throws -> Self {
@@ -40,13 +40,16 @@ extension Decodable {
 }
 
 extension Encodable {
-    public func encodeToJSONData() throws -> Data {
-        let data = try JSONEncoder.shared.encode(self)
+    public func encodeToJSONData(outputFormatting: JSONEncoder.OutputFormatting = .prettyPrinted) throws -> Data {
+        let data = try JSONEncoder.create(outputFormatting: outputFormatting).encode(self)
         return data
     }
     
-    public func storeToJSON(file: URL) throws {
-        let data = try encodeToJSONData()
+    public func storeToJSON(
+        file: URL,
+        outputFormatting: JSONEncoder.OutputFormatting = .prettyPrinted
+    ) throws {
+        let data = try encodeToJSONData(outputFormatting: outputFormatting)
         let dir = file.deletingLastPathComponent()
         try fm.createDirectory(at: dir)
         try data.write(to: file, options: [.atomic])
