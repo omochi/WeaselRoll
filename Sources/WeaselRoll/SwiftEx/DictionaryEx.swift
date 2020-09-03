44b6181f33120f3a0ccd6e8a1287abad71184a30
omochimetaru <omochi.metaru@gmail.com>
2020-09-03 14:51:12 +0900

    public

--- Sources/WeaselRoll/SwiftEx/DictionaryEx.swift
+++ Sources/WeaselRoll/SwiftEx/DictionaryEx.swift
@@ -1,5 +1,5 @@
 extension Dictionary {
-    func mustGet(for key: Key) throws -> Value {
+    public func mustGet(for key: Key) throws -> Value {
         guard let value = self[key] else {
             throw MessageError("no value for key: key=\(key)")
         }
