d41fd50da5ac679cdc8cabf8185e16f2a0d9098d
omochimetaru <omochi.metaru@gmail.com>
2021-04-15 19:07:37 +0900

    ja en

--- Sources/WeaselRoll/Date/LocaleEx.swift
+++ Sources/WeaselRoll/Date/LocaleEx.swift
@@ -2,4 +2,6 @@ import Foundation
 
 extension Locale {
     public static let posix = Locale(identifier: "en_US_POSIX")
+    public static let en = Locale(identifier: "en")
+    public static let ja = Locale(identifier: "ja")
 }
