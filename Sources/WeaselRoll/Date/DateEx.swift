064e9cfb19ec9bac603726658fa8b903d74d160f
omochimetaru <omochi.metaru@gmail.com>
2021-08-06 15:38:08 +0900

    available

--- Sources/WeaselRoll/Date/DateEx.swift
+++ Sources/WeaselRoll/Date/DateEx.swift
@@ -9,6 +9,7 @@ extension Date {
         formatter.string(from: self)
     }
 
+    @available(iOS 10.0, macOS 10.12, tvOS 10.0, *)
     public func format(_ formatter: ISO8601DateFormatter) -> String {
         formatter.string(from: self)
     }
