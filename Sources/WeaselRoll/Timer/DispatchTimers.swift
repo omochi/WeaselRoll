91f9a8cad3a5db17e42063cdccd5820c6e2afafc
omochimetaru <omochi.metaru@gmail.com>
2021-12-03 14:35:10 +0900

    dispatch timers

--- Sources/WeaselRoll/Timer/DispatchTimers.swift
+++ Sources/WeaselRoll/Timer/DispatchTimers.swift
@@ -1,13 +1,12 @@
 import Dispatch
 
-extension DispatchSource {
-    public static func startTimer(
-        flags: TimerFlags = [],
+public enum DispatchTimers {
+    public static func start(
+        flags: DispatchSource.TimerFlags = [],
         queue: DispatchQueue?,
         deadline: DispatchTime,
-        handler: @escaping () -> Void) ->
-        DispatchSourceTimer
-    {
+        handler: @escaping () -> Void
+    ) -> DispatchSourceTimer {
         let timer = DispatchSource.makeTimerSource(flags: flags, queue: queue)
         timer.schedule(deadline: deadline)
         timer.setEventHandler(handler: handler)
